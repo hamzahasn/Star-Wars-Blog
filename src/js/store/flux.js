a25e905fc48c4b7a7b19f0ad2e9d01f12aac1c88
hamzahasn <hamza.hasnain@gmail.com>
2020-12-18 02:18:06 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,4 +1,5 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	const baseUrl = "https://www.swapi.tech/api";
 	return {
 		store: {
 			demo: [
@@ -12,17 +13,38 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			planets: [],
+			people: [],
+			vehicles: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			fetchData: () => {
+				fetch(`${baseUrl}/planets`)
+					.then(res => {
+						if (!res.ok) throw new Error(res.statusText);
+						return res.json();
+					})
+					.then(data => setStore({ planets: data.results }))
+					.catch(err => console.error(err));
+				fetch(`${baseUrl}/people`)
+					.then(res => {
+						if (!res.ok) throw new Error(res.statusText);
+						return res.json();
+					})
+					.then(data => setStore({ people: data.results }))
+					.catch(err => console.error(err));
+				fetch(`${baseUrl}/vehicles`)
+					.then(res => {
+						if (!res.ok) throw new Error(res.statusText);
+						return res.json();
+					})
+					.then(data => setStore({ vehicles: data.results }))
+					.catch(err => console.error(err));
 			},
 			changeColor: (index, color) => {
 				//get the store
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Card } from "../component/card";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import { Context } from "../store/appContext";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	const buildRow = data =>
+		data.map((item, index) => {
+			return (
+				<div className="col" key={index}>
+					<Card img={""} title={item.name} description={""} btnLink={""} btnText="View more..." />
+				</div>
+			);
+		});
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<div className="row">{buildRow(store.planets)}</div>
+			<a href="#" className="btn btn-success">
+				If you see this green button, bootstrap is working
+			</a>
+		</div>
+	);
+};
